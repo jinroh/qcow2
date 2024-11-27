ff7391b2f40a770b9b2f61fe12b6f5eafbaeb326
Pierre Guilleminot <pierre.guilleminot@datadoghq.com>
2024-11-27 22:12:05 +0100

    readonly and onefile

--- qcow2.go
+++ qcow2.go
@@ -18,10 +18,13 @@
 package qcow2
 
 import (
+	"compress/flate"
+	"encoding/binary"
 	"fmt"
 	"io"
-	"os"
+	"math"
 	"sync"
+	"unsafe"
 
 	"github.com/goburrow/cache"
 )
@@ -34,7 +37,7 @@ const (
 
 type Image struct {
 	mu          sync.RWMutex
-	f           *os.File
+	f           io.ReaderAt
 	hdr         *HeaderAndAdditionalFields
 	tableCache  cache.LoadingCache
 	clusterSize int64
@@ -42,38 +45,7 @@ type Image struct {
 	cursor      int64
 }
 
-func Create(path string, size int64) (*Image, error) {
-	f, err := os.Create(path)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := writeHeader(f, size); err != nil {
-		_ = f.Close()
-		return nil, err
-	}
-
-	if err := f.Close(); err != nil {
-		return nil, err
-	}
-
-	return Open(path, false)
-}
-
-func Open(path string, readOnly bool) (*Image, error) {
-	var f *os.File
-	var err error
-
-	if readOnly {
-		f, err = os.OpenFile(path, os.O_RDONLY, 0o444)
-	} else {
-		f, err = os.OpenFile(path, os.O_RDWR, 0o644)
-	}
-
-	if err != nil {
-		return nil, err
-	}
-
+func Open(f io.ReaderAt) (*Image, error) {
 	hdr, err := readHeader(f)
 	if err != nil {
 		return nil, err
@@ -92,19 +64,79 @@ func Open(path string, readOnly bool) (*Image, error) {
 	return i, nil
 }
 
-func (i *Image) Close() error {
-	return i.f.Close()
+func readHeader(r io.ReaderAt) (*HeaderAndAdditionalFields, error) {
+	var hdr Header
+	f := io.NewSectionReader(r, 0, math.MaxInt64)
+	if err := binary.Read(f, binary.BigEndian, &hdr); err != nil {
+		return nil, fmt.Errorf("failed to read image header: %w", err)
+	}
+
+	if hdr.Magic != Magic {
+		return nil, fmt.Errorf("invalid magic bytes")
+	}
+
+	if hdr.Version != Version3 {
+		return nil, fmt.Errorf("only version 3 is supported")
+	}
+
+	if hdr.BackingFileOffset != 0 {
+		return nil, fmt.Errorf("backing files are not supported")
+	}
+
+	if hdr.CryptMethod != NoEncryption {
+		return nil, fmt.Errorf("encryption is not supported")
+	}
+
+	if hdr.IncompatibleFeatures != 0 {
+		return nil, fmt.Errorf("incompatible features are not supported")
+	}
+
+	var additionalFields *HeaderAdditionalFields
+	if hdr.HeaderLength > uint32(unsafe.Sizeof(hdr)) {
+		additionalFields = &HeaderAdditionalFields{}
+		if err := binary.Read(f, binary.BigEndian, additionalFields); err != nil {
+			return nil, fmt.Errorf("failed to read additional header fields: %w", err)
+		}
+	}
+
+	if additionalFields != nil && additionalFields.CompressionType != CompressionTypeDeflate {
+		return nil, fmt.Errorf("unsupported compression type")
+	}
+
+	var extensions []HeaderExtension
+	for {
+		var headerExtension HeaderExtension
+		if err := binary.Read(f, binary.BigEndian, &headerExtension.HeaderExtensionMetadata); err != nil {
+			return nil, fmt.Errorf("failed to read header extension type and length: %w", err)
+		}
+
+		if headerExtension.Type == EndOfHeaderExtensionArea {
+			break
+		}
+
+		if headerExtension.Type == BackingFileFormatName ||
+			headerExtension.Type == ExternalDataFileName ||
+			headerExtension.Type == FullDiskEncryptionHeader {
+			return nil, fmt.Errorf("unsupported header extension")
+		}
+
+		headerExtension.Data = make([]byte, headerExtension.Length)
+		if _, err := io.ReadFull(f, headerExtension.Data); err != nil {
+			return nil, fmt.Errorf("failed to read header extension data: %w", err)
+		}
+
+		extensions = append(extensions, headerExtension)
+	}
+
+	return &HeaderAndAdditionalFields{
+		Header:           hdr,
+		AdditionalFields: additionalFields,
+		Extensions:       extensions,
+	}, nil
 }
 
 func (i *Image) Size() (int64, error) {
 	return int64(i.hdr.Size), nil
-}
-
-func (i *Image) Sync() error {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-
-	return i.f.Sync()
 }
 
 func (i *Image) Read(p []byte) (n int, err error) {
@@ -152,60 +184,159 @@ func (i *Image) ReadAt(p []byte, diskOffset int64) (n int, err error) {
 	return
 }
 
-func (i *Image) Write(p []byte) (n int, err error) {
-	i.cursorMu.Lock()
-	defer i.cursorMu.Unlock()
-
-	n, err = i.WriteAt(p, i.cursor)
-	i.cursor += int64(n)
-	return
+type tableKey struct {
+	imageOffset int64
+	n           int
 }
 
-func (i *Image) WriteAt(p []byte, diskOffset int64) (n int, err error) {
-	i.mu.Lock()
-	defer i.mu.Unlock()
+func (i *Image) tableLoader(key cache.Key) (cache.Value, error) {
+	imageOffset := key.(tableKey).imageOffset
+	n := key.(tableKey).n
 
-	n = len(p)
-	if n == 0 {
-		return
+	buf := make([]byte, 8*n)
+	if _, err := i.f.ReadAt(buf, imageOffset); err != nil {
+		return nil, fmt.Errorf("failed to read table: %w", err)
 	}
 
-	if diskOffset+int64(n) > int64(i.hdr.Size) {
-		err = io.ErrUnexpectedEOF
-		return
+	t := make([]uint64, n)
+	for i := range t {
+		t[i] = binary.BigEndian.Uint64(buf[i*8 : (i+1)*8])
 	}
 
-	remaining := n
-	for remaining > 0 {
-		w, err := i.clusterWriter(diskOffset)
-		if err != nil {
-			return n - remaining, err
-		}
-
-		bytesInCluster, err := w.Write(p[:min(int64(i.clusterSize), int64(remaining))])
-		if err != nil && err != io.EOF {
-			return n - remaining, err
-		}
-
-		// advance to the next cluster.
-		diskOffset += int64(bytesInCluster)
-		p = p[bytesInCluster:]
-		remaining -= bytesInCluster
-	}
-
-	return
+	return t, nil
 }
 
-// Snapshots are not implemented yet but we have some scaffolding in place.
-func (i *Image) Snapshot() error {
-	i.mu.Lock()
-	defer i.mu.Unlock()
-
-	if err := i.incrementRefcounts(int64(i.hdr.L1TableOffset), int(i.hdr.L1Size)); err != nil {
-		return fmt.Errorf("failed to increment refcounts: %w", err)
+func (i *Image) readTable(imageOffset int64, n int) ([]uint64, error) {
+	t, err := i.tableCache.Get(tableKey{imageOffset: imageOffset, n: n})
+	if err != nil {
+		return nil, fmt.Errorf("failed to read table: %w", err)
 	}
 
-	return nil
+	return t.([]uint64), nil
+}
+
+func (i *Image) getRefcount(diskOffset int64) (uint64, error) {
+	refcountOffset, err := i.diskToRefcountOffset(diskOffset)
+	if err != nil {
+		return 0, err
+	}
+
+	refcountBits := int64(1 << i.hdr.RefcountOrder)
+	return readBits(i.f, refcountOffset, refcountBits)
+}
+
+func (i *Image) diskToRefcountOffset(diskOffset int64) (int64, error) {
+	refcountBits := int64(1 << i.hdr.RefcountOrder)
+
+	refcountBlockEntries := i.clusterSize * 8 / refcountBits
+
+	refcountBlockIndex := (diskOffset / i.clusterSize) % refcountBlockEntries
+	refcountTableIndex := (diskOffset / i.clusterSize) / refcountBlockEntries
+
+	refCountTableEntries := (int64(i.hdr.RefcountTableClusters) * i.clusterSize) / 8
+	refCountTable, err := i.readTable(int64(i.hdr.RefcountTableOffset), int(refCountTableEntries))
+	if err != nil {
+		return 0, err
+	}
+
+	refcountBlockOffset := int64(refCountTable[refcountTableIndex] &^ ((1 << 9) - 1))
+
+	return refcountBlockOffset + refcountBlockIndex*refcountBits, nil
+}
+
+func readBits(f io.ReaderAt, imageOffset int64, nBits int64) (uint64, error) {
+	nBytes := (nBits + 7) / 8
+	buf := make([]byte, nBytes)
+
+	if _, err := f.ReadAt(buf, imageOffset); err != nil {
+		return 0, fmt.Errorf("failed to read bits: %w", err)
+	}
+
+	var bits uint64
+	for bitIdx := 0; bitIdx < int(nBits); bitIdx++ {
+		bits <<= 1
+		byteIdx := bitIdx / 8
+		bitPosition := 7 - (bitIdx % 8)
+		if buf[byteIdx]&(1<<bitPosition) != 0 {
+			bits |= 1
+		}
+	}
+
+	return bits, nil
+}
+
+func (i *Image) clusterReader(diskOffset int64) (io.Reader, error) {
+	bytesRemainingInCluster := i.clusterSize - (diskOffset % i.clusterSize)
+
+	l2Entries := i.clusterSize / 8
+	l2Index := (diskOffset / i.clusterSize) % l2Entries
+	l1Index := (diskOffset / i.clusterSize) / l2Entries
+
+	l1Table, err := i.readTable(int64(i.hdr.L1TableOffset), int(i.hdr.L1Size))
+	if err != nil {
+		return nil, err
+	}
+
+	l1Entry := L1TableEntry(l1Table[l1Index])
+
+	l2TableOffset := l1Entry.Offset()
+
+	l2Table, err := i.readTable(l2TableOffset, int(l2Entries))
+	if err != nil {
+		return nil, err
+	}
+
+	l2Entry := L2TableEntry(l2Table[l2Index])
+
+	// Is it a hole?
+	if l2Entry.Unallocated() {
+		return io.LimitReader(zeroReader{}, int64(bytesRemainingInCluster)), nil
+	}
+
+	// Is it a compressed cluster?
+	if l2Entry.Compressed() {
+		imageOffset := l2Entry.Offset(i.hdr)
+
+		fr := flate.NewReader(io.NewSectionReader(i.f, imageOffset, l2Entry.CompressedSize(i.hdr)))
+
+		if _, err := io.CopyN(io.Discard, fr, diskOffset%i.clusterSize); err != nil {
+			return nil, err
+		}
+
+		return io.LimitReader(fr, int64(bytesRemainingInCluster)), nil
+	}
+
+	imageOffset := l2Entry.Offset(i.hdr) + (diskOffset % i.clusterSize)
+
+	return io.NewSectionReader(i.f, imageOffset, int64(bytesRemainingInCluster)), nil
+}
+
+func (i *Image) diskToImageOffset(diskOffset int64) (int64, L2TableEntry, error) {
+	clusterSize := int64(1 << i.hdr.ClusterBits)
+
+	l2Entries := clusterSize / 8
+	l2Index := (diskOffset / clusterSize) % l2Entries
+	l1Index := (diskOffset / clusterSize) / l2Entries
+
+	l1Table, err := i.readTable(int64(i.hdr.L1TableOffset), int(i.hdr.L1Size))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	l1Entry := L1TableEntry(l1Table[l1Index])
+
+	l2Table, err := i.readTable(l1Entry.Offset(), int(l2Entries))
+	if err != nil {
+		return 0, 0, err
+	}
+
+	l2Entry := L2TableEntry(l2Table[l2Index])
+
+	return l2Entry.Offset(i.hdr) + (diskOffset % clusterSize), l2Entry, nil
+}
+
+func (i *Image) alignToClusterBoundary(offset int64) int64 {
+	return i.clusterSize * (offset / i.clusterSize)
 }
 
 func min(a, b int64) int64 {
@@ -213,4 +344,14 @@ func min(a, b int64) int64 {
 		return a
 	}
 	return b
+}
+
+// zeroReader is a reader that reads zeros.
+type zeroReader struct{}
+
+func (zeroReader) Read(p []byte) (int, error) {
+	for i := range p {
+		p[i] = 0
+	}
+	return len(p), nil
 }
